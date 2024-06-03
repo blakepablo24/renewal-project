71e4de53feb85cf5f9343fcf853009cc1cac641e
Paul Robson <blakepablo24@gmail.com>
2024-06-03 20:50:51 +0100

    added social media links

--- src/components/Ui/Footer/Footer.jsx
+++ src/components/Ui/Footer/Footer.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
-import './Footer.css';
+import classes from './Footer.module.css';
 import { FaTwitter, FaLinkedin, FaFacebookSquare } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { FaPhone, FaAt } from "react-icons/fa";
 
 const footer = (props) => {
     return(
-        <div className='Footer'>
-            <div className="policy-container">
-                <div className='contact-information'>
+        <div className={classes.Footer}>
+            <div className={classes.policyContainer}>
+                <div className={classes.contactInformation}>
                     <FaPhone />
                     <p>07795304633</p>
                 </div>
-                <div className='contact-information'>
+                <div className={classes.contactInformation}>
                     <FaAt />
                     <p>info@renewal-project.com</p>
                 </div>
             </div>
-            <div className='links'>
-                <FaFacebookSquare />
-                <FaTwitter />
-                <FaLinkedin />
+            <div className={classes.footerLinks}>
+                <Link className={classes.footerLink} to="https://web.facebook.com/renewalprojectofficial"><FaFacebookSquare /></Link>
+                <Link className={classes.footerLink} to="https://x.com/renewalpuk"><FaTwitter /></Link>
+                <Link className={classes.footerLink} to="https://www.linkedin.com/company/renewal-project-official"><FaLinkedin /></Link>
             </div>
-            <div className="policy-container">
+            <div className={classes.policyContainer}>
                 <Link to="/cookie-policy" ><p>Cookie Policy</p></Link>
                 <Link to="/privacy-policy" ><p>Privacy Policy</p></Link>
             </div>
-            <p className='footer-header'>Rights reserved renewal project.</p>
+            <p className={classes.footerHeader}>Rights reserved renewal project.</p>
         </div>
     )
 }
